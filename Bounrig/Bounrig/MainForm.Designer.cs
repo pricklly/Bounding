994284b9c437e1e132514ecc6e69c0f6e00e4f28
Aleksandr Kiber <36310292+pricklly@users.noreply.github.com>
2024-04-18 12:33:34 +0300

    Add files via upload

---
+++ Bounrig/Bounrig/MainForm.Designer.cs
@@ -0,0 +1,111 @@
+﻿/*
+ * Создано в SharpDevelop.
+ * Пользователь: pricklly
+ * Дата: 18.04.2024
+ * Время: 12:00
+ * 
+ * Для изменения этого шаблона используйте меню "Инструменты | Параметры | Кодирование | Стандартные заголовки".
+ */
+namespace Bounrig
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		private System.Windows.Forms.RichTextBox rtb;
+		private System.Windows.Forms.TextBox tb;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.ProgressBar pb;
+		private System.Windows.Forms.Button button2;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.rtb = new System.Windows.Forms.RichTextBox();
+			this.tb = new System.Windows.Forms.TextBox();
+			this.button1 = new System.Windows.Forms.Button();
+			this.pb = new System.Windows.Forms.ProgressBar();
+			this.button2 = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// rtb
+			// 
+			this.rtb.Location = new System.Drawing.Point(4, 4);
+			this.rtb.Name = "rtb";
+			this.rtb.Size = new System.Drawing.Size(339, 242);
+			this.rtb.TabIndex = 0;
+			this.rtb.Text = "";
+			// 
+			// tb
+			// 
+			this.tb.Location = new System.Drawing.Point(3, 264);
+			this.tb.Name = "tb";
+			this.tb.Size = new System.Drawing.Size(339, 20);
+			this.tb.TabIndex = 1;
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(367, 253);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(78, 31);
+			this.button1.TabIndex = 2;
+			this.button1.Text = "Send";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.Button1Click);
+			// 
+			// pb
+			// 
+			this.pb.Location = new System.Drawing.Point(10, 318);
+			this.pb.Name = "pb";
+			this.pb.Size = new System.Drawing.Size(331, 33);
+			this.pb.Step = 1;
+			this.pb.TabIndex = 3;
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(367, 317);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(78, 34);
+			this.button2.TabIndex = 4;
+			this.button2.Text = "Pushing";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.Button2Click);
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(564, 380);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.pb);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.tb);
+			this.Controls.Add(this.rtb);
+			this.Name = "MainForm";
+			this.Text = "Bounrig";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+	}
+}
---
+++ Bounrig/Bounrig/MainForm.cs
@@ -0,0 +1,72 @@
+﻿
+/*
+ * Создано в SharpDevelop.
+ * Пользователь: pricklly
+ * Дата: 18.04.2024
+ * Время: 12:00
+ * 
+ * Для изменения этого шаблона используйте меню "Инструменты | Параметры | Кодирование | Стандартные заголовки".
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Bounrig
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		public int[,] bt=new int[10,10]{{123,345,234,234,24,234,46,234,457,25},{345,35,345,54,57,24,35,345,24,24},
+			{234,57,456,678,456,68,456,25,68,35},{678,46,68,643,68,46,46,35,46,365},
+			{345,57,453,456,57,35663,75,57,63,364},{24,46,24,4636,36,2452,36,36,25,46},
+			{463,475,53,36,36,57,36,46,36,46},{345,234,46,254,36,25,234,4365,35,24},
+			{453,35,475,435,345,24,57,46,35,25},{242,345,24,35,24,46,24,6,24,25}};
+		public int i,j,x;
+		public long c;
+		public Random rnd;
+		public bool s=true;
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+		void Button1Click(object sender, EventArgs e)
+		{
+			rtb.AppendText(tb.Text+'\n'.ToString());
+		}
+		void Button2Click(object sender, EventArgs e)
+		{
+			rnd=new Random();
+			for (i=0;i<10;i++)
+			{
+				for (j=0;j<10;j++)
+				{
+					rtb.AppendText(bt[i,j].ToString()+"  ");
+					c=c+bt[i,j];
+				}
+				rtb.AppendText('\n'.ToString());
+				x=c.ToString().Length;
+				c=c-(c%(x-3));
+				rtb.AppendText("1/"+c.ToString());
+			}
+			
+			while(s==true)
+			{
+				if (pb.Value<100)
+				{
+				pb.Value= pb.Value+rnd.Next(8);
+				} else 
+				{s=false;}
+			}
+		}
+	}
+}
